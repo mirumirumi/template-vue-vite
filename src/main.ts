a1f6f4a06a17847a2eb42dd190a992117ae0f03e
mirumi <info@milmemo.net>
2022-03-30 10:50:15 +0900

    [update] : 2022/3/30

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@
 // @ts-nocheck@
 
 import App from "./App.vue"
-import axios from "axios"
-import VueAxios from "vue-axios"
 import router from "./router/router"
 import Toaster from "@/components/modules/vue-toaster"
 import { createApp } from "vue"
@@ -12,7 +10,6 @@ import { createPinia } from "pinia"
 const app = createApp(App)
   .use(router)
   .use(createPinia())
-  .use(VueAxios, axios)
   .mount("#app")
 
 // app.use(Toaster, {
